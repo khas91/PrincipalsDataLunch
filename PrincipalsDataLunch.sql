f175a4a43c5d49c81eeaae8f1702590c60a0dc8d
Stuart Pierson <spierson@fscj.edu>
2017-05-31 09:56:54 -0400

    About to change from using StateSubmission in [Last/First Term at FSCJ] to using the class file

--- PrincipalsDataLunch.sql
+++ PrincipalsDataLunch.sql
@@ -1,67 +1,97 @@
-IF OBJECT_ID('tempdb..#temp') IS NOT NULL
-	DROP TABLE #temp
-IF OBJECT_ID('tempdb..#temp2') IS NOT NULL
-	DROP TABLE #temp2
-IF OBJECT_ID('tempdb..#temp3') IS NOT NULL
-	DROP TABLE #temp3
-IF OBJECT_ID('tempdb..#temp4') IS NOT NULL
-	DROP TABLE #temp4
+IF OBJECT_ID('tempdb..#duvalAndNassauHSGrads') IS NOT NULL
+	DROP TABLE #duvalAndNassauHSGrads
+IF OBJECT_ID('tempdb..#hsGradsWithEthnicity') IS NOT NULL
+	DROP TABLE #hsGradsWithEthnicity
+IF OBJECT_ID('tempdb..#highschools') IS NOT NULL
+	DROP TABLE #highschools
+IF OBJECT_ID('tempdb..#devedeligibility') IS NOT NULL
+	DROP TABLE #devedeligibility
+IF OBJECT_ID('tempdb..#devedexempted') IS NOT NULL
+	DROP TABLE #devedexempted
+IF OBJECT_ID('tempdb..#pelleligibility') IS NOT NULL
+	DROP TABLE #pelleligibility	
+IF OBJECT_ID('tempdb..#prevSSNs') IS NOT NULL
+	DROP TABLE #prevSSNs
+IF OBJECT_ID('tempdb..#duals') IS NOT NULL
+	DROP TABLE #duals
+IF OBJECT_ID('tempdb..#degrank') IS NOT NULL
+	DROP TABLE #degrank
+IF OBJECT_ID('tempdb..#highestdegearned') IS NOT NULL
+	DROP TABLE #highestdegearned
+		
 
-
-
+		
 SELECT DISTINCT
-	inst.FLA_STATE_HS_CODE
-	,inst.INST_NM
-	,cred.ACT_GRAD_DT
-	,cred.STDNT_ID
-	,cred.DIPL_TYPE
-	,stdnt.SEX
-	,CASE 
-		WHEN ISNULL(MAX(deved.[DE1050-HSCHOOL-DEV]), 'Never Attended') = '' THEN 'Pre SB1720'
-		ELSE ISNULL(MAX(deved.[DE1050-HSCHOOL-DEV]), 'Never Attended') 
-	END AS [DE1050-HSCHOOL-DEV]
-	,ISNULL(SUM(CAST(r8.[3209_AidAmount] AS INT)), 0) AS [Aid Awarded]
+	gen.FIELD_VALUE AS AWD_TYPE
+	,CAST(gen2.FIELD_VALUE AS INT) AS DEGRANK
 INTO
-	#temp
+	#degrank
+FROM
+	MIS.dbo.UTL_CODE_TABLE_120 code
+	INNER JOIN MIS.dbo.UTL_CODE_TABLE_GENERIC_120 gen ON gen.ISN_UTL_CODE_TABLE = code.ISN_UTL_CODE_TABLE
+	INNER JOIN MIS.dbo.ST_PROGRAMS_A_136 prog ON prog.AWD_TY = gen.FIELD_VALUE
+	INNER JOIN MIS.dbo.UTL_CODE_TABLE_GENERIC_120 gen2 ON gen2.ISN_UTL_CODE_TABLE = code.ISN_UTL_CODE_TABLE
+WHERE
+	code.TABLE_NAME = 'AWARD-LVL'
+	AND code.STATUS = 'A'
+	AND gen.cnxarraycolumn = 0
+	AND gen2.cnxarraycolumn = 7
+	AND prog.EFF_TRM_D <> ''
+	AND prog.END_TRM = ''
+
+SELECT
+	cred.STDNT_ID
+	,MAX(inst.INST_ID) AS [instID]
+INTO
+	#highschools
 FROM
 	MIS.dbo.ST_EXTRNL_CRDNTL_A_141 cred
 	INNER JOIN MIS.dbo.ST_INSTITUTION_A_166 inst ON inst.INST_ID = cred.INST_ID
 	INNER JOIN MIS.dbo.ST_STDNT_A_125 stdnt ON stdnt.STUDENT_SSN = cred.STDNT_ID
-	LEFT JOIN MIS.dbo.ST_STDNT_A_PREV_STDNT_SSN_USED_125 prev ON prev.ISN_ST_STDNT_A = stdnt.ISN_ST_STDNT_A
-	LEFT JOIN (SELECT
-					r1.[STUDENT-ID], xwalk.OrionTerm, r1.[DE1050-HSCHOOL-DEV]
-					,ROW_NUMBER() OVER (PARTITION BY r1.[STUDENT-ID] ORDER BY xwalk.OrionTerm DESC) RN
-				FROM
-					StateSubmission.SDB.RecordType1 r1
-					INNER JOIN MIS.dbo.vwTermYearXwalk xwalk ON xwalk.StateReportingTerm = r1.[TERM-ID]) deved ON deved.[STUDENT-ID] IN (stdnt.STUDENT_SSN, prev.PREV_STDNT_SSN)
-	LEFT JOIN StateSubmission.SDB.RecordType8 r8 ON r8.[1021_StudentId] IN (stdnt.STUDENT_SSN, prev.PREV_STDNT_SSN)
-												 AND CAST(CAST(LEFT(cred.ACT_GRAD_DT, 4) AS INT) + 1 AS VARCHAR) = r8.[3202_FinancialAidAcademicYear]
-												 AND r8.[3208_AidType] = '101'
 WHERE
-	LEFT(inst.FLA_STATE_HS_CODE, 2) IN ('16','45')
+	cred.CRDNTL_CD = 'HC'
+	AND LEFT(inst.FLA_STATE_HS_CODE, 2) IN ('16','45')
 	AND SUBSTRING(cred.ACT_GRAD_DT, 5, 2) IN ('05','06')
-	AND LEFT(cred.ACT_GRAD_DT, 4) IN ('2014','2013','2015','2016')
 	AND inst.PUBLIC_PRIVATE_IND = 'S'
-	AND deved.RN = 1
-	AND cred.CRDNTL_CD = 'HC'
+	AND LEFT(cred.ACT_GRAD_DT, 4) IN ('2014','2013','2015','2016')
 GROUP BY
-	inst.FLA_STATE_HS_CODE
+	cred.STDNT_ID
+
+SELECT
+	cred.STDNT_ID
+	,inst.FLA_STATE_HS_CODE
 	,inst.INST_NM
-	,cred.STDNT_ID
-	,stdnt.SEX
-	,LEFT(cred.ACT_GRAD_DT, 4)
 	,cred.ACT_GRAD_DT
-	--,r8.[3209_AidAmount]
-	--,deved.[DE1050-HSCHOOL-DEV]
 	,cred.DIPL_TYPE
-ORDER BY
-	ACT_GRAD_DT
+	,stdnt.SEX
+	,stdnt.ETHNICITY
+	,stdnt.ISN_ST_STDNT_A
+INTO
+	#duvalAndNassauHSGrads
+FROM
+	#highschools h
+	INNER JOIN MIS.dbo.ST_EXTRNL_CRDNTL_A_141 cred ON cred.INST_ID = h.instID
+												   AND cred.STDNT_ID = h.STDNT_ID
+	INNER JOIN MIS.dbo.ST_INSTITUTION_A_166 inst ON inst.INST_ID = cred.INST_ID
+	INNER JOIN MIS.dbo.ST_STDNT_A_125 stdnt ON stdnt.STUDENT_SSN = cred.STDNT_ID
+WHERE
+	cred.CRDNTL_CD = 'HC'
+	AND LEFT(inst.FLA_STATE_HS_CODE, 2) IN ('16','45')
+	AND SUBSTRING(cred.ACT_GRAD_DT, 5, 2) IN ('05','06')
+	AND inst.PUBLIC_PRIVATE_IND = 'S'
+	AND LEFT(cred.ACT_GRAD_DT, 4) IN ('2014','2013','2015','2016')
 
 
 SELECT
-	t.*
+	hsgrads.ISN_ST_STDNT_A
+	,hsgrads.STDNT_ID
+	,hsgrads.FLA_STATE_HS_CODE
+	,hsgrads.INST_NM
+	,hsgrads.ACT_GRAD_DT
+	,hsgrads.DIPL_TYPE
+	,hsgrads.SEX
 	,CASE
-		WHEN stdnt.ETHNICITY = 'H' THEN 'H'
+		WHEN hsgrads.ETHNICITY = 'H' THEN 'H'
 		WHEN [W] + [A] + [B] + [I] + [P] > 1 THEN 'M'
 		WHEN [W] = 1 THEN 'W'
 		WHEN [A] = 1 THEN 'A'
@@ -70,185 +100,267 @@ SELECT
 		WHEN [P] = 1 THEN 'P'
 		ELSE 'X'
 	END AS [Ethnicity]
-	,CASE
-		WHEN MAX(demo.STDNT_ID) IS NULL THEN 'Not Exempt'
-		ELSE 'Exempt'
-	END AS [Dev Ed Exempted]
 INTO
-	#temp2
+	#hsGradsWithEthnicity
 FROM
-	#temp t
-	LEFT JOIN MIS.dbo.ST_STDNT_TEST_DEMO_A_174 demo ON demo.STDNT_ID = t.STDNT_ID
-													AND demo.PLACEMENT_AREA IN ('REA','MAT','ENG')
-													AND demo.STATUS_IND IN ('F','Z')
-	LEFT JOIN MIS.dbo.ST_STDNT_A_125 stdnt ON stdnt.STUDENT_SSN = t.STDNT_ID
-	LEFT JOIN (SELECT ISN_ST_STDNT_A, RACE FROM MIS.dbo.ST_STDNT_A_RACE_125) race PIVOT (COUNT (RACE) FOR RACE IN ([W],[A],[B],[I],[X],[P])) AS racepivot ON racepivot.ISN_ST_STDNT_A = stdnt.ISN_ST_STDNT_A
-GROUP BY
-	t.STDNT_ID
-	,t.FLA_STATE_HS_CODE
-	,t.ACT_GRAD_DT
-	,t.[DE1050-HSCHOOL-DEV]
-	,t.INST_NM
-	,t.[Aid Awarded]
-	,t.SEX
-	,[W],[A],[B],[I],[X],[P]
-	,stdnt.ETHNICITY
-	,t.DIPL_TYPE
+	#duvalAndNassauHSGrads hsgrads
+	LEFT JOIN (SELECT ISN_ST_STDNT_A, RACE FROM MIS.dbo.ST_STDNT_A_RACE_125) race PIVOT (COUNT (RACE) FOR RACE IN ([W],[A],[B],[I],[X],[P])) AS racepivot ON racepivot.ISN_ST_STDNT_A = hsgrads.ISN_ST_STDNT_A
 
-SELECT
-	t.*
-	,CASE
-		WHEN LEFT(t.ACT_GRAD_DT, 4) = '2016' AND isir17.WF_IS_C_PELL_ELIG IS NOT NULL THEN 'Eligible'
-		WHEN LEFT(t.ACT_GRAD_DT, 4) = '2015' AND isir16.WF_IS_C_PELL_ELIG IS NOT NULL THEN 'Eligible'
-		WHEN LEFT(t.ACT_GRAD_DT, 4) = '2014' AND isir15.WF_IS_C_PELL_ELIG IS NOT NULL THEN 'Eligible'
-		WHEN LEFT(t.ACT_GRAD_DT, 4) = '2013' AND isir14.WF_IS_C_PELL_ELIG IS NOT NULL THEN 'Eligible'
-		ELSE 'Not Pell Eligible'
-	END AS [Pell Eligible]
-	,CASE
-		WHEN MAX(dual.OrionTerm) IS NULL THEN 'Never'
-		ELSE MAX(dual.OrionTerm)
-	END AS [Last Dual Enrolled Term]
-	,CASE
-		WHEN MIN(notdual.OrionTerm) IS NULL THEN 'Never'
-		ELSE MIN(notdual.OrionTerm)
-	END AS [First FSCJ Term]
-	,CASE
-		WHEN MIN(notdual.OrionTerm) IS NULL THEN 'Never'
-		ELSE MIN(notdual.OrionTerm)
-	END AS [Last FSCJ Term]
-INTO 
-	#temp3
+SELECT DISTINCT
+	stdnt.*
+	,ISNULL(CAST(CASE
+		WHEN deved.[STUDENT-ID] IS NOT NULL THEN MAX(deved.[DE1050-HSCHOOL-DEV])
+		WHEN MAX(deved2.[DE1050-HSCHOOL-DEV]) = '' THEN 'Pre-SB1720'
+		ELSE MAX(deved2.[DE1050-HSCHOOL-DEV])
+	END AS VARCHAR(MAX)), 'Pre-SB1720')  AS [Dev Ed Exemption Eligibility]
+INTO
+	#devedeligibility
 FROM
-	#temp2 t
-	LEFT JOIN MIS.dbo.WW_STUDENT_822 finaidst ON finaidst.WW_ST_SSN = t.STDNT_ID
-	LEFT JOIN MIS.dbo.WF_ISIR_1617_932 isir17 ON isir17.WW_STUDENT_ID = finaidst.WW_STUDENT_ID
-											  AND isir17.WF_IS_C_PELL_ELIG = 'Y'
-	LEFT JOIN MIS.dbo.WF_ISIR_1516_927 isir16 ON isir16.WW_STUDENT_ID = finaidst.WW_STUDENT_ID
-											  AND isir16.WF_IS_C_PELL_ELIG = 'Y'
-	LEFT JOIN MIS.dbo.WF_ISIR_1415_922 isir15 ON isir15.WW_STUDENT_ID = finaidst.WW_STUDENT_ID
-											  AND isir15.WF_IS_C_PELL_ELIG = 'Y'
-	LEFT JOIN MIS.dbo.WF_ISIR_1314_917 isir14 ON isir14.WW_STUDENT_ID = finaidst.WW_STUDENT_ID
-											  AND isir14.WF_IS_C_PELL_ELIG = 'Y'
-	LEFT JOIN MIS.dbo.WF_ISIR_1213_911 isir13 ON isir13.WW_STUDENT_ID = finaidst.WW_STUDENT_ID
-											  AND isir13.WF_IS_C_PELL_ELIG = 'Y'
-	LEFT JOIN MIS.dbo.ST_STDNT_A_125 stdnt ON stdnt.STUDENT_SSN = t.STDNT_ID
+	#hsGradsWithEthnicity stdnt
 	LEFT JOIN MIS.dbo.ST_STDNT_A_PREV_STDNT_SSN_USED_125 prev ON prev.ISN_ST_STDNT_A = stdnt.ISN_ST_STDNT_A
 	LEFT JOIN (SELECT
-					r6.DE1021, xwalk.OrionTerm
+					r1.[STUDENT-ID]
+					,r1.[DE1050-HSCHOOL-DEV]
+					,ROW_NUMBER() OVER (PARTITION BY r1.[STUDENT-ID] ORDER BY xwalk.OrionTerm DESC) RN
 				FROM
-					StateSubmission.SDB.RecordType6 r6
-					INNER JOIN MIS.dbo.vwTermYearXwalk xwalk ON xwalk.StateReportingTerm = r6.DE1028
-				WHERE
-					r6.DE3004 <> 'NN') dual ON dual.DE1021 IN (stdnt.STUDENT_SSN, prev.PREV_STDNT_SSN)
+					StateSubmission.SDB.RecordType1 r1
+					INNER JOIN MIS.dbo.vwTermYearXwalk xwalk ON xwalk.StateReportingTerm = r1.[TERM-ID]) deved ON deved.[STUDENT-ID] = stdnt.STDNT_ID
+																											   AND deved.RN = 1
 	LEFT JOIN (SELECT
-					r6.DE1021, xwalk.OrionTerm
+					r1.[STUDENT-ID]
+					,r1.[DE1050-HSCHOOL-DEV]
+					,ROW_NUMBER() OVER (PARTITION BY r1.[STUDENT-ID] ORDER BY xwalk.OrionTerm DESC) RN
 				FROM
-					StateSubmission.SDB.RecordType6 r6
-					INNER JOIN MIS.dbo.vwTermYearXwalk xwalk ON xwalk.StateReportingTerm = r6.DE1028
-				WHERE
-					r6.DE3004 = 'NN') notdual ON notdual.DE1021 IN (stdnt.STUDENT_SSN, prev.PREV_STDNT_SSN)
+					StateSubmission.SDB.RecordType1 r1
+					INNER JOIN MIS.dbo.vwTermYearXwalk xwalk ON xwalk.StateReportingTerm = r1.[TERM-ID]) deved2 ON deved2.[STUDENT-ID] = prev.PREV_STDNT_SSN
+																											   AND deved2.RN = 1
 GROUP BY
-	t.STDNT_ID
-	,t.FLA_STATE_HS_CODE
-	,t.ACT_GRAD_DT
-	,t.[DE1050-HSCHOOL-DEV]
-	,t.INST_NM
-	,t.[Aid Awarded]
-	,t.SEX
-	,t.Ethnicity
-	,t.DIPL_TYPE
-	,t.[Dev Ed Exempted]
-	,isir17.WF_IS_C_PELL_ELIG
-	,isir16.WF_IS_C_PELL_ELIG
-	,isir15.WF_IS_C_PELL_ELIG
-	,isir14.WF_IS_C_PELL_ELIG
+	stdnt.ISN_ST_STDNT_A
+	,stdnt.STDNT_ID
+	,stdnt.FLA_STATE_HS_CODE
+	,stdnt.INST_NM
+	,stdnt.ACT_GRAD_DT
+	,stdnt.DIPL_TYPE
+	,stdnt.SEX
+	,stdnt.Ethnicity
+	,deved.[STUDENT-ID]
+
+SELECT DISTINCT
+	d.*
+	,CASE
+		WHEN demo.STATUS_IND IS NOT NULL THEN 'Exempted'
+		ELSE 'Not Exempted'
+	END AS [Dev Ed Exempted]
+	,CAST(CAST(LEFT(d.ACT_GRAD_DT, 4) AS INT) + 1 AS VARCHAR) AS finAidYear
+INTO
+	#devedexempted
+FROM
+	#devedeligibility d
+	LEFT JOIN MIS.dbo.ST_STDNT_TEST_DEMO_A_174 demo ON demo.STDNT_ID = d.STDNT_ID
+													AND demo.PLACEMENT_AREA IN ('REA','MAT','ENG')
+													AND demo.STATUS_IND IN ('F','Z')
+
+SELECT DISTINCT
+	t.*
+	,CASE
+		WHEN (t.finAidYear = '2017' AND isir17.ISN_WF_ISIR_1617 IS NULL) OR
+			 (t.finAidYear = '2016' AND isir16.ISN_WF_ISIR_1516 IS NULL) OR
+			 (t.finAidYear = '2015' AND isir15.ISN_WF_ISIR_1415 IS NULL) OR
+			 (t.finAidYear = '2014' AND isir14.ISN_WF_ISIR_1314 IS NULL) THEN 'No FAFSA'
+		WHEN (t.finAidYear = '2017' AND isir17.WF_IS_C_PELL_ELIG = 'Y') OR
+			 (t.finAidYear = '2016' AND isir16.WF_IS_C_PELL_ELIG = 'Y') OR
+			 (t.finAidYear = '2015' AND isir15.WF_IS_C_PELL_ELIG = 'Y') OR
+			 (t.finAidYear = '2014' AND isir14.WF_IS_C_PELL_ELIG = 'Y') THEN 'Pell Eligible'
+		WHEN (t.finAidYear = '2017' AND isir17.WF_IS_C_PELL_ELIG IS NULL) OR
+			 (t.finAidYear = '2016' AND isir16.WF_IS_C_PELL_ELIG IS NULL) OR
+			 (t.finAidYear = '2015' AND isir15.WF_IS_C_PELL_ELIG IS NULL) OR
+			 (t.finAidYear = '2014' AND isir14.WF_IS_C_PELL_ELIG IS NULL) THEN 'Not Pell Eligible'
+	END AS [Pell Eligible]
+INTO
+	#pelleligibility
+FROM
+	#devedexempted t
+	LEFT JOIN MIS.dbo.WF_ISIR_1617_932 isir17 ON isir17.WF_IS_SSN = t.STDNT_ID
+	LEFT JOIN MIS.dbo.WF_ISIR_1516_927 isir16 ON isir16.WF_IS_SSN = t.STDNT_ID
+	LEFT JOIN MIS.dbo.WF_ISIR_1415_922 isir15 ON isir15.WF_IS_SSN = t.STDNT_ID
+	LEFT JOIN MIS.dbo.WF_ISIR_1314_917 isir14 ON isir14.WF_IS_SSN = t.STDNT_ID
+	LEFT JOIN MIS.dbo.WF_ISIR_1213_911 isir13 ON isir13.WF_IS_SSN = t.STDNT_ID
+
 
 SELECT
-	t.STDNT_ID
-	,t.FLA_STATE_HS_CODE
-	,t.ACT_GRAD_DT
-	,t.[DE1050-HSCHOOL-DEV]
-	,t.INST_NM
-	,t.[Aid Awarded]
-	,t.SEX
-	,t.Ethnicity
-	,t.DIPL_TYPE
-	,t.[Dev Ed Exempted]
-	,t.[Pell Eligible]
-	,t.[Last Dual Enrolled Term]
-	,t.[First FSCJ Term]
-	,t.[Last FSCJ Term]
-	,ISNULL(degrank.AWD_TYPE, 'None Achieved') AS [Highest Award Achieved at FSCJ]
+	pell.*, ssn1.PREV_STDNT_SSN AS [prevSSN1], ssn2.PREV_STDNT_SSN AS [prevSSN2], ssn3.PREV_STDNT_SSN AS [prevSSN3], ssn4.PREV_STDNT_SSN AS [prevSSN4]
 INTO
-	#temp4
+	#prevSSNs
+FROM
+	#pelleligibility pell
+	LEFT JOIN MIS.dbo.ST_STDNT_A_PREV_STDNT_SSN_USED_125 ssn1 ON ssn1.ISN_ST_STDNT_A = pell.ISN_ST_STDNT_A
+															  AND ssn1.cnxarraycolumn = 0
+	LEFT JOIN MIS.dbo.ST_STDNT_A_PREV_STDNT_SSN_USED_125 ssn2 ON ssn2.ISN_ST_STDNT_A = pell.ISN_ST_STDNT_A
+															  AND ssn2.cnxarraycolumn = 1
+	LEFT JOIN MIS.dbo.ST_STDNT_A_PREV_STDNT_SSN_USED_125 ssn3 ON ssn3.ISN_ST_STDNT_A = pell.ISN_ST_STDNT_A
+															  AND ssn3.cnxarraycolumn = 2
+	LEFT JOIN MIS.dbo.ST_STDNT_A_PREV_STDNT_SSN_USED_125 ssn4 ON ssn4.ISN_ST_STDNT_A = pell.ISN_ST_STDNT_A
+															  AND ssn4.cnxarraycolumn = 3
+
+SELECT
+	prev.*
+	,ISNULL(MAX(dual.OrionTerm), 'Never') AS [Last Term Dual Enrolled]
+	,ISNULL(MIN(nondual.OrionTerm), 'Never') AS [First Term At FSCJ]
+	,ISNULL(MAX(nondual.OrionTerm), 'Never') AS [Last Term At FSCJ]
+INTO
+	#duals
+FROM
+	#prevSSNs prev
+	LEFT JOIN (SELECT
+					r6.*
+					,xwalk.OrionTerm					
+				FROM
+					StateSubmission.SDB.RecordType6 r6
+					INNER JOIN MIS.dbo.vwTermYearXwalk xwalk ON xwalk.StateReportingTerm = r6.DE1028
+				WHERE
+					r6.DE3004 <> 'NN') dual ON dual.DE1021 IN (prev.STDNT_ID, prev.prevSSN1, prev.prevSSN2, prev.prevSSN3, prev.prevSSN4)
+	LEFT JOIN (SELECT
+					r6.*
+					,xwalk.OrionTerm					
+				FROM
+					StateSubmission.SDB.RecordType6 r6
+					INNER JOIN MIS.dbo.vwTermYearXwalk xwalk ON xwalk.StateReportingTerm = r6.DE1028
+				WHERE
+					r6.DE3004 = 'NN') nondual ON nondual.DE1021 IN (prev.STDNT_ID, prev.prevSSN1, prev.prevSSN2, prev.prevSSN3, prev.prevSSN4)
+GROUP BY
+	prev.ISN_ST_STDNT_A
+	,prev.ACT_GRAD_DT
+	,prev.[Dev Ed Exempted]
+	,prev.[Dev Ed Exemption Eligibility]
+	,prev.DIPL_TYPE
+	,prev.Ethnicity
+	,prev.finAidYear
+	,prev.FLA_STATE_HS_CODE
+	,prev.INST_NM
+	,prev.[Pell Eligible]
+	,prev.STDNT_ID
+	,prev.SEX
+	,prev.prevSSN1
+	,prev.prevSSN2
+	,prev.prevSSN3
+	,prev.prevSSN4
+
+SELECT
+	*
+FROM
+	#duals
+
+SELECT
+	 SRC.ISN_ST_STDNT_A
+	,SRC.ACT_GRAD_DT
+	,SRC.[Dev Ed Exempted]
+	,SRC.[Dev Ed Exemption Eligibility]
+	,SRC.DIPL_TYPE
+	,SRC.Ethnicity
+	,SRC.finAidYear
+	,SRC.FLA_STATE_HS_CODE
+	,SRC.INST_NM
+	,SRC.[Pell Eligible]
+	,SRC.STDNT_ID
+	,SRC.SEX
+	,SRC.prevSSN1
+	,SRC.prevSSN2
+	,SRC.prevSSN3
+	,SRC.prevSSN4
+	,SRC.[Last Term Dual Enrolled]
+	,SRC.[First Term At FSCJ]
+	,SRC.[Last Term At FSCJ]
+	,SRC.AWD_TYPE AS [Highest Degree Earned]
+INTO 
+	#highestdegearned
 FROM
 	(
 	SELECT
-		t.STDNT_ID
-		,t.FLA_STATE_HS_CODE
-		,t.ACT_GRAD_DT
-		,t.[DE1050-HSCHOOL-DEV]
-		,t.INST_NM
-		,t.[Aid Awarded]
-		,t.SEX
-		,t.Ethnicity
-		,t.DIPL_TYPE
-		,t.[Dev Ed Exempted]
-		,t.[Pell Eligible]
-		,t.[Last Dual Enrolled Term]
-		,t.[First FSCJ Term]
-		,t.[Last FSCJ Term]
-		,MAX(t.RANK) AS [MaxRank]
+		d.*
+		,deg.AWD_TYPE
+		,ROW_NUMBER() OVER (PARTITION BY d.STDNT_ID ORDER BY deg.DEGRANK DESC) RN
 	FROM
-		(
-		SELECT
-			t.*
-			,degrank.AWD_TYPE
-			,degrank.RANK
-		FROM
-			#temp3 t
-			LEFT JOIN MIS.dbo.ST_STDNT_OBJ_AWD_A_178 obj ON obj.STDNT_ID = t.STDNT_ID
-			LEFT JOIN MIS.dbo.ST_PROGRAMS_A_136 prog ON prog.PGM_CD = obj.PGM_ID
-													 AND prog.EFF_TRM_D <> ''
-													 AND prog.EFF_TRM_D <= obj.ACT_GRAD_TRM
-													 AND (prog.END_TRM = '' OR prog.END_TRM >= obj.ACT_GRAD_TRM)
-			LEFT JOIN (SELECT
-							gen1.FIELD_VALUE AS AWD_TYPE, gen2.FIELD_VALUE AS RANK
-						FROM
-							MIS.dbo.UTL_CODE_TABLE_120 code
-							LEFT JOIN MIS.dbo.UTL_CODE_TABLE_GENERIC_120 gen1 ON gen1.ISN_UTL_CODE_TABLE = code.ISN_UTL_CODE_TABLE
-																			  AND gen1.cnxarraycolumn = 0
-							LEFT JOIN MIS.dbo.UTL_CODE_TABLE_GENERIC_120 gen2 ON gen2.ISN_UTL_CODE_TABLE = code.ISN_UTL_CODE_TABLE
-																			  AND gen2.cnxarraycolumn = 7
-						WHERE
-							code.TABLE_NAME = 'AWARD-LVL'
-							AND code.STATUS = 'A') degrank ON degrank.AWD_TYPE = prog.AWD_TY) t 
-		GROUP BY
-			t.STDNT_ID
-			,t.FLA_STATE_HS_CODE
-			,t.ACT_GRAD_DT
-			,t.[DE1050-HSCHOOL-DEV]
-			,t.INST_NM
-			,t.[Aid Awarded]
-			,t.SEX
-			,t.Ethnicity
-			,t.DIPL_TYPE
-			,t.[Dev Ed Exempted]
-			,t.[Pell Eligible]
-			,t.[Last Dual Enrolled Term]
-			,t.[First FSCJ Term]
-			,t.[Last FSCJ Term]
-		) t
-		LEFT JOIN (SELECT
-						gen1.FIELD_VALUE AS AWD_TYPE, gen2.FIELD_VALUE AS RANK
-					FROM
-						MIS.dbo.UTL_CODE_TABLE_120 code
-						LEFT JOIN MIS.dbo.UTL_CODE_TABLE_GENERIC_120 gen1 ON gen1.ISN_UTL_CODE_TABLE = code.ISN_UTL_CODE_TABLE
-																		  AND gen1.cnxarraycolumn = 0
-						LEFT JOIN MIS.dbo.UTL_CODE_TABLE_GENERIC_120 gen2 ON gen2.ISN_UTL_CODE_TABLE = code.ISN_UTL_CODE_TABLE
-																		  AND gen2.cnxarraycolumn = 7
-					WHERE
-						code.TABLE_NAME = 'AWARD-LVL'
-						AND code.STATUS = 'A' ) degrank ON degrank.RANK = t.MaxRank
+		#duals d
+		LEFT JOIN MIS.dbo.ST_STDNT_OBJ_AWD_A_178 obj ON obj.STDNT_ID = d.STDNT_ID
+													 AND obj.ACT_GRAD_TRM <> ''
+		LEFT JOIN #degrank deg ON deg.AWD_TYPE = obj.AWD_TYPE) SRC
+WHERE
+	SRC.RN = 1
 
 
+SELECT
+	high.*
+	,COUNT(obj.STDNT_ID) AS [Number of Awards]
+INTO
+	#numberAwards
+FROM
+	#highestdegearned high
+	LEFT JOIN MIS.dbo.ST_STDNT_OBJ_AWD_A_178 obj ON obj.STDNT_ID = high.STDNT_ID
+												 AND obj.ACT_GRAD_TRM <> ''
+GROUP BY
+	 high.ISN_ST_STDNT_A
+	,high.ACT_GRAD_DT
+	,high.[Dev Ed Exempted]
+	,high.[Dev Ed Exemption Eligibility]
+	,high.DIPL_TYPE
+	,high.Ethnicity
+	,high.finAidYear
+	,high.FLA_STATE_HS_CODE
+	,high.INST_NM
+	,high.[Pell Eligible]
+	,high.STDNT_ID
+	,high.SEX
+	,high.prevSSN1
+	,high.prevSSN2
+	,high.prevSSN3
+	,high.prevSSN4
+	,high.[Last Term Dual Enrolled]
+	,high.[First Term At FSCJ]
+	,high.[Last Term At FSCJ]
+	,high.[Highest Degree Earned]
+
+SELECT
+	num.*
+	,CASE
+		WHEN (stdnt.CR_APPL_DT <> '' OR stdnt.VC_APPL_DT <> '' OR stdnt.BA_APPL_DT <> '' OR COUNT(obj.PGM_ID) > 0) AND num.[Last Term At FSCJ] = 'Never' THEN 'No Show'
+	END AS [No Shows]
+INTO
+	#noshows
+FROM
+	#numberAwards num
+	INNER JOIN MIS.dbo.ST_STDNT_A_125 stdnt ON stdnt.ISN_ST_STDNT_A = num.ISN_ST_STDNT_A
+	LEFT JOIN MIS.dbo.ST_STDNT_OBJ_AWD_A_178 obj ON obj.STDNT_ID = num.STDNT_ID
+												 AND obj.PGM_ID <> '3408'
+												 AND obj.EFF_TERM >  num.[Last Term Dual Enrolled]
+GROUP BY
+	 num.ISN_ST_STDNT_A
+	,num.ACT_GRAD_DT
+	,num.[Dev Ed Exempted]
+	,num.[Dev Ed Exemption Eligibility]
+	,num.DIPL_TYPE
+	,num.Ethnicity
+	,num.finAidYear
+	,num.FLA_STATE_HS_CODE
+	,num.INST_NM
+	,num.[Pell Eligible]
+	,num.STDNT_ID
+	,num.SEX
+	,num.prevSSN1
+	,num.prevSSN2
+	,num.prevSSN3
+	,num.prevSSN4
+	,num.[Last Term Dual Enrolled]
+	,num.[First Term At FSCJ]
+	,num.[Last Term At FSCJ]
+	,num.[Highest Degree Earned]
+	,num.[Number of Awards]
+	,stdnt.CR_APPL_DT
+	,stdnt.VC_APPL_DT
+	,stdnt.BA_APPL_DT
+												
+SELECT
+	*
+FROM 
+	#noshows n
+WHERE
+	[Number of Awards] > 1
